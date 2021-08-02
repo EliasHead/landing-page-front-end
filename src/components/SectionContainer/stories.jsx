0dcabca6a7569f94d24f875c9da95eaf1e36ad53
elias costa <eliashead.costa@gmail.com>
2021-08-02 15:02:02 -0300

    chore: add logolink, container, background

---
+++ src/components/SectionContainer/stories.jsx
@@ -0,0 +1,30 @@
+import { SectionContainer } from '.';
+
+export default {
+  title: 'SectionContainer',
+  component: SectionContainer,
+  args: {
+    children: (
+      <div>
+        <h1>SectionContainer</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit unde
+          excepturi possimus praesentium nulla ullam, perferendis veniam illo
+          harum esse error culpa sapiente enim magnam quos iste doloribus,
+          aperiam cum!
+        </p>
+      </div>
+    ),
+  },
+  argTypes: {
+    children: { type: '' },
+  },
+};
+
+export const Template = (args) => {
+  return (
+    <div>
+      <SectionContainer {...args} />
+    </div>
+  );
+};
