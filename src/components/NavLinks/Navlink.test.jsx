1806d79f12a1d21d413ff25694c782301ca61c1b
elias costa <eliashead.costa@gmail.com>
2021-08-12 15:59:17 -0300

    GridTwoColumn

--- src/components/NavLinks/Navlink.test.jsx
+++ src/components/NavLinks/Navlink.test.jsx
@@ -1,23 +1,23 @@
 import { screen } from '@testing-library/react';
 import { renderTheme } from '../../styles/render-theme';
-import { Navlinks } from '.';
+import { NavLinks } from '.';
 
 import mock from './mock';
 import { theme } from '../../styles/theme';
 
-describe('<Navlink />', () => {
+describe('<NavLinks />', () => {
   it('should render links', () => {
-    renderTheme(<Navlinks links={mock}>Children</Navlinks>);
+    renderTheme(<NavLinks links={mock} />);
     expect(screen.getAllByRole('link')).toHaveLength(mock.length);
   });
 
   it('should not render links', () => {
-    renderTheme(<Navlinks links={mock}>Children</Navlinks>);
-    expect(screen.queryAllByAltText(/links/i)).toHaveLength(0);
+    renderTheme(<NavLinks />);
+    expect(screen.queryAllByText(/links/i)).toHaveLength(0);
   });
 
   it('should render links', () => {
-    renderTheme(<Navlinks links={mock} />);
+    renderTheme(<NavLinks links={mock} />);
     expect(screen.getByText(/link 10/i).parentElement).toHaveStyleRule(
       'flex-flow',
       'column wrap',
@@ -28,7 +28,7 @@ describe('<Navlink />', () => {
   });
 
   it('should match snapshot', () => {
-    const { container } = renderTheme(<Navlinks links={mock} />);
+    const { container } = renderTheme(<NavLinks links={mock} />);
     expect(container.firstChild).toMatchSnapshot();
   });
 });
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div className="App">
       <Styled.Container>
-        <Heading>Hello</Heading>
+        <Heading>Menu</Heading>
       </Styled.Container>
     </div>
   );
